cc9ebac7c2939dbe1e314ec3a71290fc7169f594
mcfalljb <mcfalljb@gmail.com>
2020-02-27 16:10:14 -0500

    Added allauth and local logins need to fix profile.html and add social connections, change password, and other options to users profile

---
+++ apps/users/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from allauth.account.forms import SignupForm
+from .models import User
+
+
+class UserEditForm(forms.ModelForm):
+    """Form for viewing and editing name fields in a User object.
+
+    A good reference for Django forms is:
+    http://pydanny.com/core-concepts-django-modelforms.html
+    """
+
+    def __init__(self, *args, **kwargs):
+        # TODO: this doesn't seem to work. Need to get to the bottom of it.
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'display_name')
+
+
+class UserAdminForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'display_name', 'is_staff', 'is_active', 'date_joined')
+
+    def is_valid(self):
+        return super().is_valid()
+
+class CustomSignupForm(SignupForm):
+    display_name = forms.CharField(max_length=14, label='display name')
+    def signup(self, request, user):
+        user.display_name = self.cleaned_data['display name']        
+        user.save()
+        return user
