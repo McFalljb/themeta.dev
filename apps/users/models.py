2dde775991aa5e3332090ad3d521877b47bffa0b
mcfalljb <mcfalljb@gmail.com>
2020-04-30 06:50:15 -0400

    added postgres

--- apps/users/models.py
+++ apps/users/models.py
@@ -89,8 +89,8 @@ class User(AbstractBaseUser, PermissionsMixin):
         """
         send_mail(subject, message, from_email, [self.email])
 
-    # def __str__(self):
-    #     return self.email
+    def __str__(self):
+        return self.email
 
     # def natural_key(self):
     #     return (self.email,)
--- main/settings.py
+++ main/settings.py
@@ -127,10 +127,21 @@ SOCIALACCOUNT_PROVIDERS = {
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangometa',
+        'USER': 'mcfalljb',
+        'PASSWORD': '4418',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
