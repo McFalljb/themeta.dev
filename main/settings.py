21864170dffe91fc9a8055bd70c54140a9dee729
McFalljb <mcfalljb@gmail.com>
2021-01-20 15:31:26 -0500

    removed secrets

--- main/settings.py
+++ main/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qqj+bu44nx*%)7d2^s)-2cgzql$um$#__4t4#@2v_&lwhqpy9%'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
